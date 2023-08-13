5a7431d7e3a60327816b33c1392ab6d85a18c9c6
nghia <naovu@protonmail.com>
2023-08-13 23:39:40 +0700

    i18n back button

--- src/components/BackButton/index.jsx
+++ src/components/BackButton/index.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
+import { useTranslation } from 'react-i18next';
 
 export default function BackButton() {
 	const navigate = useNavigate();
+	const [t, i18n] = useTranslation();
 
 	return (
-
 		<Button
 			type='text'
-			
 			icon={<ArrowLeftOutlined />}
 			onClick={() => navigate(-1)}
-			style={{ color: "#F76A1A", paddingLeft: 0, marginBlock: 10}}>
-			Back
+			style={{ color: '#F76A1A', paddingLeft: 0, marginBlock: 10 }}
+		>
+			${t('Back')}
 		</Button>
 	);
 }
